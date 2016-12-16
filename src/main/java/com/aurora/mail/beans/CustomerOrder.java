c3b73f0eb5d6bec1be85f5d02c1d4df4b85cc12a
ahmad <ahmad.iqbal@jbilling.com>
2016-12-16 12:28:11 +0500

    add license header

--- src/main/java/com/aurora/mail/beans/CustomerOrder.java
+++ src/main/java/com/aurora/mail/beans/CustomerOrder.java
@@ -1,3 +1,35 @@
+/***************************************************************************** 
+* Copyright 2016 Aurora Solutions 
+* 
+*    http://www.aurorasolutions.io 
+* 
+* Aurora Solutions is an innovative services and product company at 
+* the forefront of the software industry, with processes and practices 
+* involving Domain Driven Design(DDD), Agile methodologies to build 
+* scalable, secure, reliable and high performance products.
+* 
+* The Mail Service exposes a RESTful API, developed with Spring Boot,
+* for sending emails. The service is configurable via a properties file
+* to allow for customizing configuration including SMTP mail properties.
+* The project also leverages Thymeleaf Template enigne for defining
+* HTML email templates. The API is documented using Swagger, and easily
+* viewable from the web browser using Swagger UI. Other frameworks used
+* in the project are Flying Saucer, logback, Mockito, JUnit and Wiser.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at 
+* 
+*    http://www.apache.org/licenses/LICENSE-2.0 
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+*****************************************************************************/
+
+
 package com.aurora.mail.beans;
 
 public class CustomerOrder {
